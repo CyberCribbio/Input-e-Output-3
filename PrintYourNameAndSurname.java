202204652a8d25865f1d2ca78e50ce7c670273fc
Cristian Valenti <122291593+CyberCribbio@users.noreply.github.com>
2023-01-28 19:39:21 +0100

    Add files via upload

---
+++ PrintYourNameAndSurname.java
@@ -0,0 +1,11 @@
+public class PrintYourNameAndSurname {
+    public PrintYourNameAndSurname() {
+        }
+
+        public static void main(String[] args) {
+            System.out.println("Cristian" + " " + "Salvatore");
+            System.out.println("Valenti");
+        }
+    }
+
+
